2f2e38a882274e4f8e499550fa3099b20f5cf3ea
Viet Thien <vthien3009@gmail.com>
2022-06-19 00:39:11 +0700

    bailab2

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
