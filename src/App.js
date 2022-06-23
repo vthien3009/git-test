d6b183e7725a9c1039ef3d98a64b207f463e3db4
Viet Thien <vthien3009@gmail.com>
2022-06-23 14:11:03 +0700

    LAB03_2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import "./App.css";
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponents";
+import { DISHES } from "./shared/dishes";
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -12,7 +21,7 @@ class App extends Component {
             <NavbarBrand href="">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
