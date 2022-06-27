459baf8e281b99ae5d8317e9fd896521722184d6
Viet Thien <vthien3009@gmail.com>
2022-06-28 00:46:15 +0700

    them nut tuy chinh hien thi 6,4,3 cot

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -59,6 +59,26 @@ class StaffList extends Component {
     });
     return (
       <div className="container">
+        <div className="row m-3">
+          <button
+            onClick={() => this.onColumnSelect("col-md-2 mt-1")}
+            className="btn btn-success mr-3"
+          >
+            6 cot
+          </button>
+          <button
+            onClick={() => this.onColumnSelect("col-md-3 mt-1")}
+            className="btn btn-success mr-3"
+          >
+            4 cot
+          </button>
+          <button
+            onClick={() => this.onColumnSelect("col-md-4 mt-1")}
+            className="btn btn-success mr-3"
+          >
+            3 cot
+          </button>
+        </div>
         <div className="row">{StaffList}</div>
         <div className="row mt-3">
           {this.renderStaff(this.state.onSelectedStaff)}
