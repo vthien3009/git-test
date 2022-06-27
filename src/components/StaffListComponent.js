2c5146e719401c8f58773e06fc1de02b5d3bb1d4
Viet Thien <vthien3009@gmail.com>
2022-06-28 00:24:55 +0700

    tao StaffList Compoent, render

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,1 +1,38 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import dateFormat from "dateformat";
 
+class StaffList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onSelectedStaff: null,
+      
+    };
+  }
+
+  onSelecteStaff(staff) {
+    this.setState({ onSelectedStaff: staff });
+  }
+
+
+
+
+
+  render() {
+    const StaffList = this.props.staffs.map((staff) => {
+      return (
+        <div className={this.state.columDefault}>
+          <Card key={staff.id} onClick={() => this.onSelecteStaff(staff)}>
+            <CardBody>
+              <CardTitle>{staff.name}</CardTitle>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    });
+
+
+  }
+}
+export default StaffList;
