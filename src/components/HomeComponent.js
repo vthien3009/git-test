3debdee9792b9dd764b586a6896e8393492bd4e7
Viet Thien <vthien3009@gmail.com>
2022-07-04 18:11:33 +0700

    LAB05_2

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+function Home(props){
+    return(
+        <div className="container">
+            <h4>Home</h4>
+        </div>
+    );
+}
+export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,42 +1,36 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent"
 import Menu from "./MenuComponents";
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+import {Switch, Route, Redirect} from "react-router-dom"
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
 
   render() {
     // console.log(this.state.dishes);
+    const HomePage = ()=>{
+      return (
+        <Home />
+      );
+    }
     return (
       <div className="App">
         <Header />
-        <Menu
-        // truyền vào MenuCPN dishes để render ra bên ngoài
-          dishes={this.state.dishes}
-        //   truyền toClick qua CPN con
-          toClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail
-            // truyền vào DishDetail với dish = dishes.filter (lọc ra trong dishes những dish có id tương ứng với (this.state.selectedDish) đã được thay đổi ở MenuCpn)
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
