b786599b50b8e2a3ad299bcb45c9ffacea57e9ce
Viet Thien <vthien3009@gmail.com>
2022-07-19 16:18:45 +0700

    create StaffDetail vs StaffList

---
+++ src/components/StaffList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function StaffList(props){
+    const listNhanVien = props.staffs.map((nv)=>{
+        return (
+            <Link to={`/nhanvien/${nv.id}`}>
+                <div key={nv.id}>
+                    <div className="col-12 col-lg-2 m-1">
+                        <img src={nv.image} alt={nv.name}/>
+                        <p>{nv.name}</p>
+                    </div>
+                </div>
+            </Link>
+        );
+    });
+return(
+    <div className="container">
+        <div className="row">
+                {listNhanVien}
+        </div>
+    </div>
+);
+}
+export default StaffList;
