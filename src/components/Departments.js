86dad56a29b022f374ac7819d8d1fba6df3d1678
Viet Thien <vthien3009@gmail.com>
2022-08-19 18:21:53 +0700

    cap nhat file detail.pdf + chinh sua file department (route)

--- src/components/Departments.js
+++ src/components/Departments.js
@@ -1,40 +1,53 @@
-import React, {Component} from "react";
-import {Card, CardTitle, CardBody, CardText} from "reactstrap";
-
+import React, { Component } from "react";
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardText,
+  BreadcrumbItem,
+  Breadcrumb,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 //Presentational
 class RenderDept extends Component {
-    render(){
-        return(
-
-            <Card>
-                <CardTitle className="m-2">{this.props.dept.name}</CardTitle>
-                <CardBody>
-                    <CardText>
-                        Số lượng nhân viên: {this.props.dept.numberOfStaff}
-                    </CardText>
-                </CardBody>
-            </Card>
-        );
-    }
+  render() {
+    return (
+      <Card>
+        <CardTitle className="m-2">{this.props.dept.name}</CardTitle>
+        <CardBody>
+          <CardText>
+            Số lượng nhân viên: {this.props.dept.numberOfStaff}
+          </CardText>
+        </CardBody>
+      </Card>
+    );
+  }
 }
 
 //Container component
-function Department (props) {
-    console.log(props);
-        //dung map de fetch toan bo data tu props cua Main Component
-        const departments = props.dept.map((department)=>{
-            return(
-                <div className="col-12 col-md-6 col-lg-4 mt-2 mb-2" key={department.id}>
-                    <RenderDept dept={department}/>
-                </div>
-            );
-        });
-        return(
-            <div className="container">
-                <div className="row shadow m-3">{departments}</div>
-            </div>
-        );
-        
+function Department(props) {
+  console.log(props);
+  //dung map de fetch toan bo data tu props cua Main Component
+  const departments = props.dept.map((department) => {
+    return (
+      <div className="col-12 col-md-6 col-lg-4 mt-2 mb-2" key={department.id}>
+        <RenderDept dept={department} />
+      </div>
+    );
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/nhanvien">Nhân Viên</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Phòng Ban</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+      <div className="row shadow m-3">{departments}</div>
+    </div>
+  );
 }
 export default Department;
