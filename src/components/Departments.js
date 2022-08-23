08c35756ae2d60910f238374bc4f287c2bc45018
Viet Thien <vthien3009@gmail.com>
2022-08-24 01:19:30 +0700

    hoan thanh asm4

--- src/components/Departments.js
+++ src/components/Departments.js
@@ -2,46 +2,51 @@ import React, { Component } from "react";
 import { Card, CardTitle, CardBody, CardText } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
+import { Slide, Stagger, Fade } from "react-animation-components";
 //Presentational
 class RenderDept extends Component {
   render() {
     return (
-        <Link to={`departments/${this.props.dept.id}`}>  
+      <Link to={`departments/${this.props.dept.id}`}>
         <Card>
-            <CardTitle className="m-2">{this.props.dept.name}</CardTitle>
-            <CardBody>
-            <CardText>
-                Số lượng nhân viên: {this.props.staffNo.length}
-            </CardText>
-            </CardBody>
+          <CardTitle className="m-2">{this.props.dept.name}</CardTitle>
+          <CardBody>
+            <CardText>Số lượng nhân viên: {this.props.staffNo.length}</CardText>
+          </CardBody>
         </Card>
-        </Link> 
+      </Link>
     );
   }
 }
-
 //Container component
 function Department(props) {
   console.log(props);
-  if(props.departments.isLoading){
-    return <Loading/>
+  if (props.departments.isLoading) {
+    return <Loading />;
   }
   //dung map de fetch toan bo data tu props cua Main Component
+
   const departments = props.departments.departments.map((department) => {
     return (
       <div className="col-12 col-md-6 col-lg-4 mt-2 mb-2" key={department.id}>
-        <RenderDept 
-        dept={department}
-        staffNo={props.staffs.filter((staff)=>staff.departmentId === department.id
+        <Slide in>
+          <RenderDept
+            dept={department}
+            staffNo={props.staffs.filter(
+              (staff) => staff.departmentId === department.id
             )}
-        />
+          />
+        </Slide>
       </div>
     );
   });
+
   return (
-    <div className="container">
-      <div className="row shadow m-3">{departments}</div>
-    </div>
+    <Stagger in>
+      <div className="container">
+        <div className="row shadow m-3">{departments}</div>
+      </div>
+    </Stagger>
   );
 }
 export default Department;
